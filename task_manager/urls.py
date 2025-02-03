d81e2691b4d0f6e600faa87deb45067ae1958057
4ikkamony <compbanderloga@gmail.com>
2025-02-03 23:53:17 +0200

    Update task_manager/models.py. Add views for TaskType, ProjectType, Position
    Add generic CRUD views for the simplest models(they all have just a 'name' field)
    
    Add templates for them:
      - list view templates provide links to create, update and delete views
    
    Update task_manager/urls.py, add routing to newly created views

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,85 @@
 from django.urls import path
 
-from task_manager.views import index
+from task_manager.views import (
+    index,
+    TaskTypeListView,
+    TaskTypeCreateView,
+    TaskTypeUpdateView,
+    TaskTypeDeleteView,
+    ProjectTypeListView,
+    ProjectTypeCreateView,
+    ProjectTypeUpdateView,
+    ProjectTypeDeleteView, PositionCreateView, PositionUpdateView, PositionDeleteView, PositionListView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+
+    # TaskType views
+    path(
+        "task-types/",
+        TaskTypeListView.as_view(),
+        name="task-type-list",
+    ),
+    path(
+        "task-types/create/",
+        TaskTypeCreateView.as_view(),
+        name="task-type-create",
+    ),
+    path(
+        "task-types/<int:pk>/update/",
+        TaskTypeUpdateView.as_view(),
+        name="task-type-update",
+    ),
+    path(
+        "task-types/<int:pk>/delete/",
+        TaskTypeDeleteView.as_view(),
+        name="task-type-delete",
+    ),
+
+    # ProjectType views
+    path(
+        "project-types/",
+        ProjectTypeListView.as_view(),
+        name="project-type-list",
+    ),
+    path(
+        "project-types/create/",
+        ProjectTypeCreateView.as_view(),
+        name="project-type-create",
+    ),
+    path(
+        "project-types/<int:pk>/update/",
+        ProjectTypeUpdateView.as_view(),
+        name="project-type-update",
+    ),
+    path(
+        "project-types/<int:pk>/delete/",
+        ProjectTypeDeleteView.as_view(),
+        name="project-type-delete",
+    ),
+
+    # Position views
+    path(
+        "positions/",
+        PositionListView.as_view(),
+        name="position-list",
+    ),
+    path(
+        "positions/create/",
+        PositionCreateView.as_view(),
+        name="position-create",
+    ),
+    path(
+        "positions/<int:pk>/update",
+        PositionUpdateView.as_view(),
+        name="position-update",
+    ),
+    path(
+        "positions/<int:pk>/delete/",
+        PositionDeleteView.as_view(),
+        name="position-delete",
+    ),
 ]
 
 app_name = "task_manager"
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
 from task_manager.models import (
     Team,
@@ -9,9 +12,90 @@ from task_manager.models import (
     Task,
     Position,
     TaskType,
+    ProjectType,
 )
 
 
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "task_manager/index.html")
+
+
+class TaskTypeListView(LoginRequiredMixin, generic.ListView):
+    model = TaskType
+    context_object_name = "task_type_list"
+    template_name = "task_manager/task_type_list.html"
+    paginate_by = 5
+
+
+class TaskTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = TaskType
+    template_name = "task_manager/task_type_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+
+class TaskTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = TaskType
+    template_name = "task_manager/task_type_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-type-list")
+    context_object_name = "task_type"
+
+
+class TaskTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = TaskType
+    template_name = "task_manager/task_type_confirm_delete.html"
+    success_url = reverse_lazy("task_manager:task-type-list")
+    context_object_name = "task_type"
+
+
+class ProjectTypeListView(LoginRequiredMixin, generic.ListView):
+    model = ProjectType
+    context_object_name = "project_type_list"
+    template_name = "task_manager/project_type_list.html"
+    paginate_by = 5
+
+
+class ProjectTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = ProjectType
+    template_name = "task_manager/project_type_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:project-type-list")
+
+
+class ProjectTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = ProjectType
+    template_name = "task_manager/project_type_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:project-type-list")
+    context_object_name = "project_type"
+
+
+class ProjectTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = ProjectType
+    template_name = "task_manager/project_type_confirm_delete.html"
+    success_url = reverse_lazy("task_manager:project-type-list")
+    context_object_name = "project_type"
+
+
+class PositionListView(LoginRequiredMixin, generic.ListView):
+    model = Position
+    paginate_by = 5
+
+
+class PositionCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:position-list")
+
+
+class PositionUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:position-list")
+
+
+class PositionDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Position
+    success_url = reverse_lazy("task_manager:position-list")
